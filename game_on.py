88bfafff6f64ddb061b3d97b97bbf7dc149c9c21
akhilesh-techworks <akhilesh.techworks@gmail.com>
2026-06-19 02:02:33 +0000

    some beautification

--- game_on.py
+++ game_on.py
@@ -1,18 +1,14 @@
 game_list = [0, 1, 2]
 
-
 def display_game(game_list):
     print("Here is the current game list:")
     print(game_list)
 
-
 def position_choice():
 
     choice = "wrong"
-
     while choice not in ["0", "1", "2"]:
         choice = input("Enter a position choice from (0,1,2): ")
-
         if choice not in ["0", "1", "2"]:
             print("sorry, wrong input!")
 
@@ -26,12 +22,10 @@ def replace_value(pos):
 def game_on():
 
     game_on = "Y"
-
     while game_on == "Y":
         pos = position_choice()
         replace_value(pos)
         display_game(game_list)
         game_on = input("Want to continue(Y or N): ")
-
 
 game_on()
